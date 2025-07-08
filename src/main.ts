59503bffa9e6a3e1a7d571aa9f562078b9b53afb
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-08 17:08:19 +0600

    Error handlers added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { Server } from "http";
 import app from "./app";
 import connectDatabase from "./app/config/db";
 
@@ -9,4 +10,43 @@ const databaseUri = process.env.DATABASE_URI as string;
 
 connectDatabase(databaseUri);
 
-app.listen(port, () => console.log(`App listening on port: ${port}`));
+let server: Server;
+server = app.listen(port, () => console.log(`App listening on port: ${port}`));
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal recieved. Server shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    };
+
+    process.exit(1);
+});
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled rejection detected. Server shutting down...\n", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    };
+
+    process.exit(1);
+});
+
+process.on("uncaughtException", (error) => {
+    console.log("Uncaught exception detected. Server shutting down...\n", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    };
+
+    process.exit(1);
+});
+
+// Promise.reject(new Error("Error"));
