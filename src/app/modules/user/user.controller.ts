b2063b628e7e9727514b693ae7b00c6a4cbcba0a
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-10 16:44:02 +0600

    createUserService added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,14 +1,10 @@
 import type { Request, Response } from "express";
 import { User } from "./user.model";
+import { createUserService } from "./user.service";
 
 export const createUser = async (req: Request, res: Response) => {
     try {
-        const { name, email } = req.body;
-
-        const user = await User.create({
-            name,
-            email
-        });
+        const user = await createUserService(req.body);
 
         res.status(201).json({
             success: true,
