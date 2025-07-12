aa7461ca51c6fee75f46b7c65fce53a2c8d59aaa
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-12 15:37:46 +0600

    Add validation middleware for user creation route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createUser, getUsers } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../../middlewares";
 
 const router = Router();
 
-router.post("/register", createUser);
+router.post("/register", validateRequest(createUserZodSchema), createUser);
 router.get("/", getUsers);
 
 export default router;
