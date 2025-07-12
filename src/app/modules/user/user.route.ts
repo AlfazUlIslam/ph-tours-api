79ccdadf0db7b7f336972e08c04230faf84468db
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-13 00:15:49 +0600

    Add validation middleware to updateUser route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createUser, getUsers, updateUser } from "./user.controller";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares";
 import { Role } from "./user.interface";
 import { checkAuth } from "../../middlewares";
@@ -9,6 +9,6 @@ const router = Router();
 
 router.post("/register", validateRequest(createUserZodSchema), createUser);
 router.get("/", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), getUsers);
-router.patch("/:id", checkAuth(...Object.values(Role)), updateUser);
+router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), updateUser);
 
 export default router;
