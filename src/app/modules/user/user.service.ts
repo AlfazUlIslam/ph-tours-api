57033c59aa61bbcd3497276875351620b0ec69ab
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-12 17:05:36 +0600

    Modify IAuthProvider & createUserService

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,10 +1,30 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import bcryptjs from "bcryptjs";
 
 export const createUserService = async (payload: Partial<IUser>) => {
-    const { name, email } = payload;
+    const { email, password, ...rest } = payload;
 
-    const user = await User.create({ name, email });
+    const userExists = await User.findOne({email});
+
+    if (userExists) {
+        throw new AppError(404, "User already exists");
+    };
+
+    const hashedPassword = await bcryptjs.hash(password as string, 10);
+
+    const authProvider: IAuthProvider = {
+        provider: "credentials",
+        providerId: email as string
+    };
+
+    const user = await User.create({
+        email,
+        password: hashedPassword,
+        auths: [authProvider],
+        ...rest
+    });
 
     return user;
 };
