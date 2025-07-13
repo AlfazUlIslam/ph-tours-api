7cf00dbd34789e4866cc4a9b3f4ee3efd5945c1d
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-13 17:32:49 +0600

    Add JWT_REFRESH_SECRET AND JWT_REFRESH_EXPIRY env vars

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -8,6 +8,8 @@ interface IEnv {
     NODE_ENV: "development" | "production";
     JWT_SECRET: string;
     JWT_EXPIRY: string;
+    JWT_REFRESH_SECRET: string;
+    JWT_REFRESH_EXPIRY: string;
     BCRYPT_SALT_ROUND: string;
     SUPER_ADMIN_EMAIL: string;
     SUPER_ADMIN_PASSWORD: string;
@@ -19,7 +21,9 @@ const loadEnvVariables = (): IEnv => {
         "MONGODB_URI", 
         "NODE_ENV", 
         "JWT_SECRET", 
-        "JWT_EXPIRY", 
+        "JWT_EXPIRY",
+        "JWT_REFRESH_SECRET",
+        "JWT_REFRESH_EXPIRY", 
         "BCRYPT_SALT_ROUND",
         "SUPER_ADMIN_EMAIL",
         "SUPER_ADMIN_PASSWORD"
@@ -44,6 +48,8 @@ const loadEnvVariables = (): IEnv => {
         NODE_ENV: process.env.NODE_ENV as "development" | "production",
         JWT_SECRET: process.env.JWT_SECRET as string,
         JWT_EXPIRY: process.env.JWT_EXPIRY as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_EXPIRY as string,
         BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
         SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
         SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string
