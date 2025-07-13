b37091479516c8a4342f8e127606479cc9185850
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-13 21:59:39 +0600

    feat(app): integrate cookie-parser middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import router from "./app/routes";
 import { globalErrorHandler, notFound } from "./app/middlewares"; 
 
 const app = express();
 
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
