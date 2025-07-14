424e21c2aa07b64fd628e79b7ef817c81823ecd2
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-14 19:54:45 +0600

    feat(auth): set up express-session and initialize Passport middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 import router from "./app/routes";
 import { globalErrorHandler, notFound } from "./app/middlewares"; 
 
 const app = express();
 
+app.use(expressSession({ secret: "Your secret", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
