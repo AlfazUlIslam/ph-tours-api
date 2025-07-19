4e617667060df4ec491649dd650532a626c9bf92
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-19 19:06:45 +0600

    feat(tour): add tour services, controllers, routes

---
+++ src/app/modules/tour/tour.route.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import { checkAuth, validateRequest } from "../../middlewares";
+import { Role } from "../user/user.interface";
+import { createTourZodSchema, updateTourZodSchema, createTourTypeZodSchema } from "./tour.validation";
+import { createTourType, updateTourType, getAllTourTypes, deleteTourType, getAllTours, createTour, updateTour, deleteTour } from "./tour.controller";
+
+const router = Router();
+
+/* ------------------ TOUR TYPE ROUTES -------------------- */
+router.get("/tour-types", getAllTourTypes);
+
+router.post(
+    "/create-tour-type",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validateRequest(createTourTypeZodSchema),
+    createTourType
+);
+
+router.patch(
+    "/tour-types/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validateRequest(createTourTypeZodSchema),
+    updateTourType
+);
+
+router.delete("/tour-types/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), deleteTourType);
+
+/* --------------------- TOUR ROUTES ---------------------- */
+router.get("/", getAllTours);
+
+router.post(
+    "/create",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validateRequest(createTourZodSchema),
+    createTour
+);
+
+router.patch(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validateRequest(updateTourZodSchema),
+    updateTour
+);
+
+router.delete("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), deleteTour);
+
+export default router;
---
+++ src/app/modules/tour/tour.service.ts
@@ -0,0 +1,113 @@
+import { QueryBuilder } from "../../utils/QueryBuilder";
+import { tourSearchableFields } from "./tour.constant";
+import { ITour, ITourType } from "./tour.interface";
+import { Tour, TourType } from "./tour.model";
+
+export const createTourService = async (payload: ITour) => {
+    const existingTour = await Tour.findOne({ title: payload.title });
+    if (existingTour) {
+        throw new Error("A tour with this title already exists.");
+    }
+
+    // const baseSlug = payload.title.toLowerCase().split(" ").join("-")
+    // let slug = `${baseSlug}`
+
+    // let counter = 0;
+    // while (await Tour.exists({ slug })) {
+    //     slug = `${slug}-${counter++}` // dhaka-division-2
+    // }
+
+    // payload.slug = slug;
+
+    const tour = await Tour.create(payload)
+
+    return tour;
+};
+
+export const getAllToursService = async (query: Record<string, string>) => {
+    const queryBuilder = new QueryBuilder(Tour.find(), query)
+
+    const tours = await queryBuilder
+        .search(tourSearchableFields)
+        .filter()
+        .sort()
+        .fields()
+        .paginate()
+
+    // const meta = await queryBuilder.getMeta()
+
+    const [data, meta] = await Promise.all([
+        tours.build(),
+        queryBuilder.getMeta()
+    ])
+
+
+    return {
+        data,
+        meta
+    }
+};
+
+export const updateTourService = async (id: string, payload: Partial<ITour>) => {
+
+    const existingTour = await Tour.findById(id);
+
+    if (!existingTour) {
+        throw new Error("Tour not found.");
+    }
+
+    // if (payload.title) {
+    //     const baseSlug = payload.title.toLowerCase().split(" ").join("-")
+    //     let slug = `${baseSlug}`
+
+    //     let counter = 0;
+    //     while (await Tour.exists({ slug })) {
+    //         slug = `${slug}-${counter++}` // dhaka-division-2
+    //     }
+
+    //     payload.slug = slug
+    // }
+
+    const updatedTour = await Tour.findByIdAndUpdate(id, payload, { new: true });
+
+    return updatedTour;
+};
+
+export const deleteTourService = async (id: string) => {
+    return await Tour.findByIdAndDelete(id);
+};
+
+export const createTourTypeService = async (payload: ITourType) => {
+    // const existingTourType = await TourType.findOne({ name: payload.name });
+    const existingTourType = await TourType.findOne({ name: payload });
+
+    if (existingTourType) {
+        throw new Error("Tour type already exists.");
+    }
+
+    // return await TourType.create({ name: payload.name });
+    return await TourType.create({ name: payload });
+};
+
+export const getAllTourTypesService = async () => {
+    return await TourType.find();
+};
+
+export const updateTourTypeService = async (id: string, payload: ITourType) => {
+    const existingTourType = await TourType.findById(id);
+    if (!existingTourType) {
+        throw new Error("Tour type not found.");
+    }
+
+    const updatedTourType = await TourType.findByIdAndUpdate(id, payload, { new: true });
+    return updatedTourType;
+};
+
+export const deleteTourTypeService = async (id: string) => {
+    const existingTourType = await TourType.findById(id);
+    if (!existingTourType) {
+        throw new Error("Tour type not found.");
+    }
+
+    return await TourType.findByIdAndDelete(id);
+};
