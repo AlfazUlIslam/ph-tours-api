b9a551a5995f1b9bf6abf2200e695bad985eff88
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-20 17:14:26 +0600

    feat(modules): add types and models for booking and payment modules

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from "mongoose";
+import { BookingStatus, IBooking } from "./booking.interface";
+
+
+const bookingSchema = new Schema<IBooking>({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    tour: {
+        type: Schema.Types.ObjectId,
+        ref: "Tour",
+        required: true
+    },
+    payment: {
+        type: Schema.Types.ObjectId,
+        ref: "Payment",
+        required: true
+    },
+    status: {
+        type: String,
+        enum: Object.values(BookingStatus),
+        default: BookingStatus.PENDING
+    },
+    guestCount: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+export const Booking = model<IBooking>("Booking", bookingSchema);
---
+++ src/app/modules/payment/payment.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+import { IPayment, PaymentStatus } from "./payment.interface";
+
+const paymentSchema = new Schema<IPayment>({
+    booking: {
+        type: Schema.Types.ObjectId,
+        ref: "Booking",
+        required: true,
+        unique: true
+    },
+    transactionId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    status: {
+        type: String,
+        enum: Object.values(PaymentStatus),
+        default: PaymentStatus.UNPAID
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    paymentGatewayData: {
+        type: Schema.Types.Mixed
+    },
+    invoiceUrl: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+export const Payment = model<IPayment>("Payment", paymentSchema);
