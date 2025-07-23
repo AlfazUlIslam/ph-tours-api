8c59dcd2db253feae245c572bbb5b6eb16ea5d8a
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-23 09:57:22 +0600

    feat(env): add cloudinary env variables and update loadEnvVariables, .env.example

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -29,6 +29,11 @@ interface IEnv {
         SSL_SUCCESS_FRONTEND_URL: string,
         SSL_FAIL_FRONTEND_URL: string,
         SSL_CANCEL_FRONTEND_URL: string,
+    },
+    CLOUDINARY: {
+        CLOUDINARY_CLOUD_NAME: string,
+        CLOUDINARY_API_KEY: string,
+        CLOUDINARY_API_SECRET: string
     }
 };
 
@@ -59,6 +64,9 @@ const loadEnvVariables = (): IEnv => {
         "SSL_SUCCESS_FRONTEND_URL",
         "SSL_FAIL_FRONTEND_URL",
         "SSL_CANCEL_FRONTEND_URL",
+        "CLOUDINARY_CLOUD_NAME",
+        "CLOUDINARY_API_KEY",
+        "CLOUDINARY_API_SECRET"
     ];
 
     requiredEnvVariables.forEach((key) => {
@@ -101,6 +109,11 @@ const loadEnvVariables = (): IEnv => {
             SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
             SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
             SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string
+        },
+        CLOUDINARY: {
+            CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+            CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+            CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string
         }
     };
 };
