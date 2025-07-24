38a1c4399e5539ee73c5e0ad8a3fdb6624a4cf0e
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-24 20:36:17 +0600

    fix(middleware): handle nested request body in validateRequest middleware

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -4,6 +4,7 @@ import type { AnyZodObject } from "zod";
 const validateRequest = (zodSchema: AnyZodObject) =>
     async (req: Request, res: Response, next: NextFunction) => {
         try {
+            req.body = JSON.parse(req.body.data) || req.body;
             req.body = await zodSchema.parseAsync(req.body);
             next();
         } catch (error) {
