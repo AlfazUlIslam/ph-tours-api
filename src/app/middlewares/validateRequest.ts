9dab1d8a323327f52f4df4e00de9394e834cfc50
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-24 23:35:55 +0600

    fix(validateRequest): only parse req.body.data when it exists

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -4,7 +4,10 @@ import type { AnyZodObject } from "zod";
 const validateRequest = (zodSchema: AnyZodObject) =>
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            req.body = JSON.parse(req.body.data) || req.body;
+            if (req.body.data) {
+                req.body = JSON.parse(req.body.data);
+            };
+
             req.body = await zodSchema.parseAsync(req.body);
             next();
         } catch (error) {
