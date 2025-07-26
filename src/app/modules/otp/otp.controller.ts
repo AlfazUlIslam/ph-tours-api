ba51545fb221e617f0b7855c7f87ad09fd0861a6
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-27 00:57:07 +0600

    feat(otp): add send otp feature

--- src/app/modules/otp/otp.controller.ts
+++ src/app/modules/otp/otp.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 import { asyncHandler, sendResponse } from "../../utils";
+import { sendOtpService } from "./otp.service";
 
 export const sendOtp = asyncHandler(
     async (req: Request, res: Response) => {
-        
+        const { email, name } = req.body;
+        await sendOtpService(email, name);
+
         sendResponse(res, {
             statusCode: 200,
             success: true,
